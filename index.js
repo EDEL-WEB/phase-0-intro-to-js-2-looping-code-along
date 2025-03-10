24afd45c154a2a8bfd411c12c7f5a274a7cde1f5
edel <edel.omondi@student.moringaschool.com>
2025-03-10 17:28:25 +0300

    completed 1 lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, birthday) {
+    let thankYouCards = []; // Initialize an empty array
+
+    for (let i = 0; i < cards.length; i++) {
+        thankYouCards.push(`Thank you, ${cards[i]}, for the wonderful ${birthday} gift!`);
+    }
+
+    return thankYouCards;
+}
+
+console.log(writeCards(cards, "surprise"));
+
+function countDown(){
+let n = 10;
+while  (n >= 0) {
+  console.log(n);
+  n--
+}
+}
